d3b510023240b6f4f3864810456e4480114062c7
TeresaCampo <terry.campo.2002@gmail.com>
2023-07-04 16:32:44 +0200

    TERRY PUSH

--- src/main/java/com/example/gestionaleTesina/controllers/MyTextField.java
+++ src/main/java/com/example/gestionaleTesina/controllers/MyTextField.java
@@ -24,11 +24,11 @@ public class MyTextField {
     public static void maxLen45(TextField tf){
         tf.setTextFormatter(new TextFormatter<>(change-> {
                     if (change.getControlNewText().length() <= 45) {
-                        tf.setStyle("-fx-background-color: #e7ebff");
+                        tf.setStyle("-fx-background-color: #e7ebff; -fx-background-radius: 25px; -fx-border-color: #022757; -fx-border-radius: 25px;");
                         return change;
                     }
                     else {
-                        tf.setStyle("-fx-background-color: pink");
+                        tf.setStyle("-fx-background-color: pink; -fx-background-radius: 25px; -fx-border-color: #022757; -fx-border-radius: 25px;");
                         return null;
                     }
                 })
@@ -39,7 +39,8 @@ public class MyTextField {
         pf.setTextFormatter(new TextFormatter<>(change-> {
                     if (change.getControlNewText().length() <= 20) return change;
                     else {
-                        pf.setStyle("-fx-background-color: pink");
+                        pf.setStyle("-fx-background-color: pink; -fx-background-radius: 25px; -fx-border-color: #022757; -fx-border-radius: 25px;");
+                        pf.setStyle("-fx-background-color: pink; -fx-background-radius: 25px; -fx-border-color: #022757; -fx-border-radius: 25px;");
                         return null;
                     }
                 })
